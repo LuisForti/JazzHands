faaeef85ba362fe2b2d67cf9c108eb766c5b4689
Luís <cc20806@g.unicamp.br>
2022-05-31 11:09:55 -0300

    Mudança de valores
    Após diversos testes ainda estamos com uma falha no acelerômetro: caso o jogador mova o celular rápido demais ele acaba funcionando indevidamente. Ainda iremos buscar uma maneira de corrigir. Por hora a única mudança foi nos valores dos giros, que foram de 10/-10 para 9.18/-9.18. Esses novos valores transformam o retorno do acelerômetro (m/s^2) para graus. Esse valor foi pego ao dividir 90°/9,8 m/s^2 = 9,18 ° * s^2 / m.

--- core/src/com/mygdx/tcc/MyGdxGame.java
+++ core/src/com/mygdx/tcc/MyGdxGame.java
@@ -3,8 +3,11 @@ package com.mygdx.tcc;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
@@ -27,11 +30,10 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void render () {
 		frame += 1;
 
-		if (Gdx.input.isPeripheralAvailable(Input.Peripheral.Accelerometer)) {
-			giroX = Gdx.input.getAccelerometerX() * 10;
-			giroY = Gdx.input.getAccelerometerY() * (-10);
-			giroZ = Gdx.input.getAccelerometerZ() * 10;
-		}
+		//Pega o valor do acelerômetro, em m/s^2, e converte para graus
+		giroX = Gdx.input.getAccelerometerX() * 9.18;
+		giroY = Gdx.input.getAccelerometerY() * -9.18;
+		giroZ = Gdx.input.getAccelerometerZ() * 9.18;
 
 		if(giroY < 30 && giroY > 0 && estado == 0) {
 			if(giroZ > 0) {
@@ -56,7 +58,6 @@ public class MyGdxGame extends ApplicationAdapter {
 			default: break;
 		}
 
-		//if (frame % 60 == 0) {
 		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
 		fonte.draw(batch, Byte.toString(musicaAtual), 0, 1200);
@@ -64,7 +65,6 @@ public class MyGdxGame extends ApplicationAdapter {
 		fonte.draw(batch, Double.toString(Math.round(giroY * 100) / 100), 0, 650);
 		fonte.draw(batch, Double.toString(Math.round(giroZ * 100) / 100), 0, 300);
 		batch.end();
-		//}
 	}
 	
 	@Override
