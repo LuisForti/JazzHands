ff5d5bb88e4eb41387b25f4b1dd99d3e9534ba16
Luís <cc20806@g.unicamp.br>
2022-09-15 12:20:49 -0300

    Programação da jogabilidade

--- core/src/com/jazzhands/game/JogoTcc.java
+++ core/src/com/jazzhands/game/JogoTcc.java
@@ -14,8 +14,12 @@ public class JogoTcc extends ApplicationAdapter {
     double anguloX, anguloY, anguloZ;
     String posicao = "";
     Music musicaTeste;
-    int[] batidas = {20,52,82,105,135,167,185,217,247,270,300,330,352,382,415,435,467,497,517,547,580,600,632,662,685,717,747,770,800,832,852,885,915,937,967,990,1020,1050,1072,1102,1135,1155,1187,1217,1237,1267,1300,1320,1352,1382,1405,1437,1467,1490,1520,1552,1572,1605,1635,1657,1687,1710,1740,1770,1792,1822,1855,1875,1907,1937,1957,4717,4747,4770,4800,4830,4852,4882,4915,4935,4967,4997,5017,5047,5080,5100,5132,5162,5185,5217,5247,5270,5300,5332,5352,5385,5415,5437,5467,5490,5520,5550,5572,5602,5635,5655,5687,5717,5737,5767,5800,5820,5852,5882,5905,5937,5967,5990,6020,6052,6072,6105,6135,6157,6187,6210,6240,6270,6292,6322,8962,8995,9015,9047,9077,9097,9127,9160,9180,9212,9242};
+    int[] batidas = {1};
+    int ritmo = 42;
+    int proximaBatida = 1;
     int batidaAtual = 0;
+    String[] movimentacao = {"cima", "frente"};
+    int pontuacao = 0;
 
     @Override
     public void create() {
@@ -37,7 +41,8 @@ public class JogoTcc extends ApplicationAdapter {
         anguloY = Gdx.input.getAccelerometerY() * 9.18;
         anguloZ = Gdx.input.getAccelerometerZ() * 9.18;
         // A cada 40/60 segundos ele analizará a posição do celular, aproximadamente o intervalo entre as batidas do metrônomo da música
-        if (frame == batidas[batidaAtual]) {
+        if (frame == proximaBatida) {
+            proximaBatida += ritmo;
             batidaAtual++;
             // Para cima
             if (anguloY > 40) {
@@ -45,23 +50,28 @@ public class JogoTcc extends ApplicationAdapter {
             }
             //Para trás ou para frente
             else if (anguloY < 40) {
-                if(anguloX < 40 && anguloX > -40) {
+                if (anguloX < 40 && anguloX > -40) {
                     if (anguloZ > 0)
                         posicao = "frente";
                     else
                         posicao = "trás";
-                }
-                else if(anguloX > 40)
-                {
+                } else if (anguloX > 40) {
                     posicao = "esquerda";
-                }
-                else
+                } else
                     posicao = "direita";
             }
+            if (posicao == movimentacao[batidaAtual % 2])
+            {
+                pontuacao++;
+            Gdx.input.vibrate(200);
+            }
         }
+
         ScreenUtils.clear(0, 0, 0, 1);
         batch.begin();
         fonte.draw(batch, posicao, 0, 1200);
+        fonte.draw(batch, String.valueOf(batidaAtual), 0, 400);
+        fonte.draw(batch, String.valueOf(pontuacao), 0, 800);
         batch.end();
     }
 
